3b9daeb8e5ceb7b6c2fdcf0247ede7d283f766c8
Albert Hsu <iverson52000@gmail.com>
2020-05-06 20:33:21 -0700

    final

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,81 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+const redis = require('redis');
+
+const redisClient = redis.createClient(process.env.REDIS_URI);
+
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET_KEY', { expiresIn: '2 days' });
+};
+
+
+//set token in redis and return a promise
+const setToken = (token, id) => Promise.resolve(redisClient.set(token, id));
+
+
+//create jwt token, return user data
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token, user }
+    })
+    .catch(console.log);
+};
+
+
+//handleSignin only returns promise
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+
+  return db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then((data) => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json('unable to get user'));
+          .then((user) => user[0])
+          .catch((err) => Promise.reject('unable to get user'));
       } else {
-        res.status(400).json('wrong credentials');
+        return Promise.reject('wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'));
+    .catch((err) => Promise.reject('Error!'));
 };
 
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({id: reply})
+  });
+};
+
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+
+  return authorization ? getAuthTokenId(req, res) 
+    :handleSignin(db, bcrypt, req, res)
+    .then((data) => {
+      return data.id && data.email ? createSession(data) : Promise.reject(data)
+    })
+    .then((session) => res.json(session))
+    .catch((err) => res.status(400).json(err));
+};
+
+
 module.exports = {
-  handleSignin
+  signinAuthentication,
+  redisClient
 };
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
+
 
 // This db connection object is for docker postgresql
 const db = knex({
@@ -16,20 +18,24 @@ const db = knex({
   connection: process.env.POSTGRES_URI
 });
 
+
 const app = express();
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
+
 app.get('/', (req, res)=> { res.send('it is working') });
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
-app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)});
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)});
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
 
 app.listen(process.env.PORT || 3001, ()=> {
-  console.log(`app is running on port ${process.env.PORT || 3001}`);
+  console.log(`app is running on port ${ process.env.PORT || 3001 }`);
 });
